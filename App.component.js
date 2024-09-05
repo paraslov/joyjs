23e27f781a8cb13913e1746bb297e41ac29fa5ed
paraslov <paraslovjm@gmail.com>
2024-09-05 20:29:12 +0500

    JJS-1 (feat): init project

---
+++ App.component.js
@@ -0,0 +1,55 @@
+export function AppComponent() {
+  const element = document.createElement('div')
+
+  const localState = {
+    page: 'todolist',
+  }
+
+  AppComponent.render({ element, localState })
+
+  return {
+    localState,
+    element,
+    // cleanUp: () => {},
+  }
+}
+
+AppComponent.render = ({element, localState, props, liba}) => {
+  console.log('App render')
+
+  const pageSelector = document.createElement('select')
+
+  const counterPageOption = document.createElement('option')
+  counterPageOption.append('Counter Page')
+  counterPageOption.value = 'counter'
+
+  const todolistOption = document.createElement('option')
+  todolistOption.append('Todo List App')
+  todolistOption.value = 'todolist'
+
+  pageSelector.append(counterPageOption, todolistOption)
+  pageSelector.value = localState.page
+
+  element.append(pageSelector)
+
+  pageSelector.addEventListener("change", () => {
+    localState.page = pageSelector.value
+
+    AppComponent.render({element, localState})
+  })
+
+  switch (localState.page) {
+    case 'counter': {
+      const counterInstance = 'COUNTER'
+      element.append(counterInstance)
+
+      break
+    }
+    case 'todolist': {
+      const todolistInstance = 'TODOLIST'
+      element.append(todolistInstance)
+
+      break
+    }
+  }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import { AppComponent } from "./App.component.js";
+
+const rootElement = document.getElementById('root')
+
+const appInstance = AppComponent()
+
+rootElement.append(appInstance.element)
