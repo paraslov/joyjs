5f39d028597b75f12c9e822be900f93c761c5c72
paraslov <paraslovjm@gmail.com>
2024-09-07 12:56:37 +0500

    JJS-2 (feat): add automatic cleanup functions execution by joyJS

--- JoyJS.js
+++ JoyJS.js
@@ -1,24 +1,40 @@
 export const Joy = {
-  create(ComponentFunction, props = {}) {
-    const componentJoyInstance = {
+  create(ComponentFunction, props = {}, { parentInstance } = { parentInstance: null }) {
+    const componentJoy = {
       refresh() {
         componentInstance.element.innerHTML = ''
+
+        if (componentInstance.childrenComponents) {
+          componentInstance.childrenComponents.forEach(cc => cc.cleanup?.())
+          componentInstance.childrenComponents = []
+        }
+
         renderComponent()
       }
     }
-    const renderJoyInstance = {
-      create: Joy.create,
-    }
-    Object.setPrototypeOf(renderJoyInstance, componentJoyInstance);
+    const renderJoy = {
+      create(ChildrenComponentFunction, props) {
+        const childrenComponentInstance = Joy.create(ChildrenComponentFunction, props, { parentInstance: componentInstance })
 
-    const componentInstance = ComponentFunction(props, { joy: componentJoyInstance })
+        return childrenComponentInstance
+      },
+    }
+    Object.setPrototypeOf(renderJoy, componentJoy); // inherit refresh method
+
+    const componentInstance = ComponentFunction(props, { joy: componentJoy })
+
+    if (parentInstance) {
+      if (!parentInstance.childrenComponents) parentInstance.childrenComponents = []
+
+      parentInstance.childrenComponents.push(componentInstance)
+    }
 
     function renderComponent() {
       ComponentFunction.render({
         element: componentInstance.element,
         localState: componentInstance.localState,
         props: componentInstance.props,
-        joy: renderJoyInstance,
+        joy: renderJoy,
       })
     }
 
