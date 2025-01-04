a0e36bca72363b6de754f171656c9e029d366d60
paraslov <paraslovjm@gmail.com>
2025-01-04 21:59:11 +0500

    JJS-3 (feat): refactor of Joy objects inside create method

--- JoyJS.js
+++ JoyJS.js
@@ -1,39 +1,18 @@
-import {checkSameProps} from "./checkSameProps.js";
+import {checkSameProps} from './checkSameProps.js'
 
 class JoyJS {
-  create(ComponentFunction, props = {}, { parentInstance } = { parentInstance: null }) {
-    const renderJoy = {
-      create(ChildrenComponentFunction, props) {
-        componentInstance.childrenIndex++
-        const cachedComponentInstance = componentInstance.childrenComponents?.[componentInstance.childrenIndex]
+  create(ComponentFunction, props = {}, {parentInstance} = {parentInstance: null}) {
+    let componentInstance = {}
 
-        if (cachedComponentInstance) {
-          const isComponentSameTypeAsItWas = cachedComponentInstance.type === ChildrenComponentFunction
+    const renderJoy = createRenderJoy(() => componentInstance, this)
+    const componentJoy = createComponentJoy(() => componentInstance, ComponentFunction, renderJoy)
 
-          if (isComponentSameTypeAsItWas) {
-            return getUpdatedComponent(cachedComponentInstance, props)
-          }
+    Object.setPrototypeOf(renderJoy, componentJoy) // inherit refresh method
 
-          delete componentInstance.childrenComponents[componentInstance.childrenIndex]
-        }
+    componentInstance = getComponentInstance(ComponentFunction, props, componentJoy)
 
-        return Joy.create(ChildrenComponentFunction, props, {parentInstance: componentInstance})
-      },
-    }
-    const componentJoy = {
-      refresh() {
-        componentInstance.element.innerHTML = ''
-
-        if (componentInstance.childrenComponents) {
-          componentInstance.childrenComponents.forEach(cc => cc.cleanup?.())
-        }
-
-        renderComponent(componentInstance, ComponentFunction, renderJoy)
-      }
-    }
-    Object.setPrototypeOf(renderJoy, componentJoy); // inherit refresh method
-
-    const componentInstance = getComponentInstance(ComponentFunction, props, componentJoy)
+    renderJoy.setComponentInstance(componentInstance)
+    componentJoy.setComponentInstance(componentInstance)
 
     if (parentInstance) {
       setParentChildrenComponents(parentInstance, componentInstance)
@@ -45,9 +24,15 @@ class JoyJS {
   }
 }
 
-function createRenderJoy (componentInstance, Joy) {
-  return {
+function createRenderJoy(getComponentInstance, Joy) {
+  let componentInstance = null
+
+  const renderJoy = {
     create(ChildrenComponentFunction, props) {
+      if (!componentInstance?.element) {
+        throw new Error('componentInstance.element is not defined.')
+      }
+
       componentInstance.childrenIndex++
       const cachedComponentInstance = componentInstance.childrenComponents?.[componentInstance.childrenIndex]
 
@@ -63,11 +48,42 @@ function createRenderJoy (componentInstance, Joy) {
 
       return Joy.create(ChildrenComponentFunction, props, {parentInstance: componentInstance})
     },
+    setComponentInstance(instance) {
+      componentInstance = instance
+    },
   }
+
+  return renderJoy
+}
+
+// Factory to create componentJoy
+function createComponentJoy(getComponentInstance, ComponentFunction, renderJoy) {
+  let componentInstance = null
+
+  const componentJoy = {
+    refresh() {
+      if (!componentInstance?.element) {
+        throw new Error('componentInstance.element is not defined.')
+      }
+
+      componentInstance.element.innerHTML = ''
+
+      if (componentInstance.childrenComponents) {
+        componentInstance.childrenComponents.forEach(cc => cc.cleanup?.())
+      }
+
+      renderComponent(componentInstance, ComponentFunction, renderJoy)
+    },
+    setComponentInstance(instance) {
+      componentInstance = instance
+    },
+  }
+
+  return componentJoy
 }
 
 function getComponentInstance(ComponentFunction, props, componentJoy) {
-  const componentInstance = ComponentFunction(props, { joy: componentJoy })
+  const componentInstance = ComponentFunction(props, {joy: componentJoy})
   componentInstance.type = ComponentFunction
   componentInstance.refresh = componentJoy.refresh
 
