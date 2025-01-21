83411f4e4b199a3f5c2201016821cc3608253575
Баланов Сергей Валерьевич <sergei.balanov@kaspi.kz>
2025-01-21 18:03:14 +0500

    JJS-6 (feat): refactor JoyJS.ts

--- src/JoyJS.ts
+++ src/JoyJS.ts
@@ -13,7 +13,8 @@ import {
   JoyProps,
   RenderJoyCreateOptions
 } from './core/types/core-types.ts';
-import { validateComponentFunction, validateComponentInstance } from './core/validations/validations.ts';
+import { JoyJsError, validateComponentFunction, validateComponentInstance } from './core/validations/validations.ts';
+import { createHtmlElement } from './core/joyJs/createHtmlElement.ts';
 
 class JoyJS {
   create(
@@ -24,19 +25,43 @@ class JoyJS {
     validateComponentFunction(ComponentFunction);
 
     const componentStates: ComponentStates = [];
+    let componentRootElement: HTMLElement | null = null;
+
     const componentJoy: ComponentJoy = {
-      useState: <T>(initialState: T) => {
+      useState<T>(initialState: T) {
         const refreshComponentFn = () => componentInstance.renderJoy.refresh();
         return useStateFactory<T>(initialState, componentStates, refreshComponentFn);
+      },
+      create(tagName, props = {}) {
+        componentRootElement = createHtmlElement(tagName, props);
+        return componentRootElement;
       }
     };
 
     const componentInstance = getComponentInstance(ComponentFunction, props, componentJoy);
+    if (!componentRootElement) {
+      throw new JoyJsError('You must call liba.create in your component function for creating root element: ' + ComponentFunction.name);
+    }
+    componentInstance.element = componentRootElement;
+    componentInstance.props = props;
 
     componentInstance.renderJoy = {
-      create: (ChildrenComponentFunction, props, options?: RenderJoyCreateOptions) =>
-        createChildComponent(componentInstance, ChildrenComponentFunction, props, options),
-      refresh: () => refreshComponent(componentInstance, componentStates)
+      create(tagNameOrComponentFn, props: any = {}, options?: RenderJoyCreateOptions): any {
+        if (isJoyType(tagNameOrComponentFn)) {
+          const newComponent = createChildComponent(componentInstance, tagNameOrComponentFn, props, options);
+          componentRootElement?.append(newComponent.element);
+
+          return newComponent;
+        } else {
+          const newElement = createHtmlElement(tagNameOrComponentFn, props);
+
+          componentRootElement?.append(newElement);
+          return newElement;
+        }
+      },
+      refresh() {
+        refreshComponent(componentInstance, componentStates);
+      }
     };
 
     validateComponentInstance(componentInstance);
@@ -51,3 +76,7 @@ class JoyJS {
 }
 
 export const Joy = new JoyJS();
+
+function isJoyType(fnOrTag: unknown): fnOrTag is JoyComponent {
+  return typeof fnOrTag === 'function';
+}
